f8e5bbca8a32918b65c782d36da8e2277ecc8c7a
Ignat36 <75880369+Ignat36@users.noreply.github.com>
2021-03-04 00:30:57 +0300

    Create ConsoleApplication1.c

---
+++ ConsoleApplication1.c
@@ -0,0 +1,20 @@
+﻿//Вариант 26
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 1; i < 1001; i++)
+    {
+        int x = i, x_square = i * i;
+
+        while (x && (x_square % 10 == x % 10))
+        {
+            x_square /= 10;
+            x /= 10;
+        }
+
+        if (!x)
+            printf("[%d] in a square is [%d]\n", i, i * i);
+    }
+    return 0;
+}
